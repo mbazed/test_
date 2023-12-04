9a14135d0148558ed03793f3fdd9e5fce6d4f8ba
Ajay V <ajuu1402@gmail.com>
2023-12-04 19:28:07 +0530

    components updated

--- client/src/components/manufacturer/ManufacturerMed.jsx
+++ client/src/components/manufacturer/ManufacturerMed.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 
 const ManufacturerMed = () => {
-    const [medicineID, setMedicineID] = useState('');
-    const [quantity, setQuantity] = useState('');
-    const [doe, setDoe] = useState('');
-    const [description, setDescription] = useState('');
+    const [medicineID, setMedicineID] = useState(" ");
+    const [quantity, setQuantity] = useState(" ");
+    const [doe, setDoe] = useState(" ");
+    const [description, setDescription] = useState(" ");
 
     const handleMedicineID = (e) => {
+        console.log(e.target.value);
         setMedicineID(e.target.value);
-        console.log(medicineID);
+        // console.log(medicineID);
     };
 
     const handleQuantity = (e) => {
@@ -17,8 +19,10 @@ const ManufacturerMed = () => {
     };
 
     const handleDoe = (e) => {
-        setDoe(e.target.value);
-        console.log(doe);
+        console.log(e.target.value);
+        let newDate = new Date(e.target.value ).toDateString()
+        console.log(newDate);
+        setDoe(newDate);
     };
 
     const handleDescription = (e) => {
@@ -28,24 +32,24 @@ const ManufacturerMed = () => {
 
     return (
         <div>
-            <h1 >Manufacture Medicine</h1>
+            <h1 className='text-6xl text-center font-bold text-blue-500 mt-16 mb-16'>Manufacture Medicine</h1>
             <div className="flex ">
                 <div className="w-1/2">
-                    <div className="p-16">
-                        <label className="p-8 " htmlFor="name">MedicineID</label>
+                    <div className="p-16 drop-shadow-xl">
+                        <label className="p-8 text-4xl font-bold" htmlFor="name">MedicineID</label>
                         <input 
-                            className=""
-                            id="name"
+                            className="text-3xl border border-black-300 rounded-md"
+                            id="id"
                             type="text"
                             onChange={handleMedicineID}
                             placeholder="Name"
                             required
                         />
                     </div>
-                    <div className="p-16">
-                        <label className="p-8" htmlFor="quantity">Quantity</label>
+                    <div className="p-16 drop-shadow-xl">
+                        <label className="p-8 text-4xl font-bold" htmlFor="quantity">Quantity</label>
                         <input 
-                            className=""
+                            className="text-3xl border border-black-300 rounded-md"
                             id="quantity"
                             type="text"
                             onChange={handleQuantity}
@@ -56,21 +60,21 @@ const ManufacturerMed = () => {
                 </div>
                 
                 <div className="w-1/2">
-                    <div className="p-16">
-                        <label className="p-8" htmlFor="doe">Date of Expiry</label>
+                    <div className="p-16 drop-shadow-xl">
+                        <label className="p-8 text-4xl font-bold" htmlFor="doe">Date of Expiry</label>
                         <input 
-                            className=""
+                            className="text-3xl border border-black-300 rounded-md"
                             id="doe"
-                            type="text"
+                            type="date"
                             onChange={handleDoe}
                             placeholder="Date of Expiry"
                             required
                         />
                     </div>
-                    <div className="p-16">
-                        <label className="p-8" htmlFor="description">Description</label>
+                    <div className="p-16 drop-shadow-xl">
+                        <label className="p-8 font-bold text-4xl" htmlFor="description">Description</label>
                         <input 
-                            className=""
+                            className="text-3xl border border-black-300 rounded-md"
                             id="description"
                             type="text"
                             onChange={handleDescription}
